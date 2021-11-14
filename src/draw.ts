6a887f15a6c5cca4dd4a3bd2a71cded4a508ab1b
K. Rodman Mannix <kevinrmannix@gmail.com>
2021-11-14 12:18:25 -0500

    paint board with color

--- src/draw.ts
+++ src/draw.ts
@@ -2,9 +2,7 @@ import { IBoard, ISpace } from './interface'
 import { twoDimensional } from './util'
 
 
-export function drawBoard(board: IBoard): string[] {
+export function drawBoard(board: IBoard): ISpace[][] {
   const ranks = twoDimensional(board.spaces).sort((as, bs) => bs[0].rank - as[0].rank)
-  return ranks.map((spaces: ISpace[]) =>
-    spaces.sort(a => a.file).map(a => a.human).join(' ')
-  )
+  return ranks.map((spaces: ISpace[]) => spaces.sort(a => a.file))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import chalk from 'chalk'
 import { Board } from './board'
 import { drawBoard } from './draw'
+import { BaseColor, ISpace } from './interface'
+
+const log = (x: string) => process.stdout.write(x)
 
 const board = new Board()
-const rows = drawBoard(board)
-console.log(rows.join('\n'))
+const twoDSpaces: ISpace[][] = drawBoard(board)
+
+twoDSpaces.forEach((files, index) => {
+  if (index) log('\n')
+  files.forEach(file => {
+    if (file.baseColor == BaseColor.Black) log(chalk.white(file.human))
+    else log(chalk.bgWhite.black(file.human))
+  })
+  if (index === twoDSpaces.length - 1) log('\n')
+})
